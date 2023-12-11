53830a4700166c704a886fcd3b8a591ef29f0667
wuk0o <wukoo@protonmail.com>
2023-12-11 19:03:56 +0800

    update

--- synccustom.py
+++ synccustom.py
@@ -4,21 +4,21 @@ import yaml
 import os
 import sys
 
-CLASH_CFG_PATH = os.path.expanduser('~/.config/clash/custom.yaml')
-CUSTOM_PATH = os.path.abspath('./custom.yaml')
+
+OUTPUT_PATH = os.path.abspath('./rules/custom.yaml')
 
 
 if __name__ == '__main__':
-    for fname in [CLASH_CFG_PATH, CUSTOM_PATH]:
+    for fname in [sys.argv[1], OUTPUT_PATH]:
         if not os.path.isfile(fname):
             print('%s not found!' % fname)
 
     rules = []
-    with open(CLASH_CFG_PATH, 'r') as fp:
+    with open(sys.argv[1], 'r') as fp:
         data = yaml.load(fp, Loader=yaml.Loader)
         rules = filter(lambda r: r.split(',')[0] in ('DOMAIN-SUFFIX', 'DOMAIN-KEYWORD'), data['rules'])
 
-    with open(CUSTOM_PATH, 'w') as fp:
+    with open(OUTPUT_PATH, 'w') as fp:
         data = {'custom-rules': list(rules)}
         yaml.dump(data, fp)
 
